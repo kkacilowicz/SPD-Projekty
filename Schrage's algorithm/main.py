591ade43bd8815f436584496068e0b05af074c2d
WJakubowska <nika99_11@o2.pl>
2021-03-30 00:51:21 +0200

    Wczytywanie zmiennych do trzech oddzielnych list i zakomentowany nie dzialajacy algorytm, musze zobaczyc czy do niego tez nie lepiej zamiast zbiorow dac listy

--- Schrage's algorithm/main.py
+++ Schrage's algorithm/main.py
@@ -12,21 +12,47 @@ file = open("SCHRAGE1.DAT", "r")
 # q - time of deliver the task
 n = 0
 
-# First elements of these lists will never be used - they're filled for better readability (problem with indexes)
-r = [0]
-p = [0]
-q = [0]
+# We make empty lists
+r = []
+p = []
+q = []
 
 if file.readable():
     n = file.readline()
     column = csv.reader(file, delimiter=' ')
     for row in column:
-        r.append(row[0])
-        p.append(row[1])
-        q.append(row[2])
+        r.append(int(row[0]))
+        p.append(int(row[1]))
+        q.append(int(row[2]))
     file.close()
 
 print(r)
 print(p)
 print(q)
 
+"""
+t = 0
+k = 0
+Cmax = 0
+G = {} # pusty zbior G
+N = {1}
+for i in range(1, int(n)+1):
+    N.add(i)  # tworzenie zbioru N
+
+while len(G) != 0 or len(N) != 0:  # dopoki zbior G lub N nie sa puste
+   while len(N) != 0  and min(r) <= t:
+        e = min(r) # e to najmniejszy element r
+        G = G | set(e)  # | to suma
+        N = N - set(e)  # - zwraca roznice zbiorow
+   if len(G) == 0:
+       t = min(r)
+       continue
+   e = max(q)
+   G = G - e
+   k=k+1
+   t = t + p
+   Cmax = max(Cmax, t + q)
+
+print(Cmax)
+
+"""
