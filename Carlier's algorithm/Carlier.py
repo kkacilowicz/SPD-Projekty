5b4e8d6e6b4c07c2c75870fc5e0db45b6b8080d7
WJakubowska <nika99_11@o2.pl>
2021-05-27 16:43:12 +0200

    Carlier's algotithm done

--- Carlier's algorithm/Carlier.py
+++ Carlier's algorithm/Carlier.py
@@ -1,4 +1,4 @@
-# Name: Carlier algorithm
+# Name: Carlier's algorithm
 # Authors: Kacper Kaciłowicz 248951, Weronika Jakubowska 248931
 # Date: 23.05.2021
 
@@ -10,16 +10,14 @@ import copy
 import math
 
 
-
-def Calier(Nset, UB):
-    NSetD = copy.deepcopy(Nset)
+def Calier(Nset):
+    global UB
+    original = copy.deepcopy(Nset)
     NSet = copy.deepcopy(Nset)
+
     opty_harmonogram = []
 
     [Harmonogram, U] = schrage.Schrage(Nset)
-    # NSetD = copy.deepcopy(Nset)
-    # NSet = copy.deepcopy(Nset)
-    # print(" na poczatku : U " + str(U) + " UB " + str(UB))
 
     if U < UB:
         UB = U
@@ -28,59 +26,58 @@ def Calier(Nset, UB):
     [a, b, c] = blok.blok(Harmonogram, U)
 
     if c == -1:
-        print(" w if : U " + str(U) + " UB " + str(UB))
+        # for i in opty_harmonogram:
+        #     print(i)
+        print(str(U))
         return opty_harmonogram, U
 
     else:
-        r = min(Harmonogram[c+1:b+1]).r
-        q = min(Harmonogram[c+1:b+1]).q
+
+        r = min(Harmonogram[c + 1:b + 1]).r
+        q = min(Harmonogram[c + 1:b + 1]).q
         p = 0
-        for i in range(c+1, b+1):
+        for i in range(c + 1, b + 1):
             p = p + Harmonogram[i].p
 
-        Harmonogram_c_r = 0
-
+        index = 0
         for i in range(0, int(n)):
-            if NSetD[i].p == Harmonogram[c].p and NSetD[i].q == Harmonogram[c].q:
-                Harmonogram_c_r = copy.copy(NSetD[i].r)
-                NSetD[i].r = max(Harmonogram[c].r, r + p)
+            if NSet[i].p == Harmonogram[c].p and NSet[i].q == Harmonogram[c].q:
+                index = i
+                NSet[i].r = max(Harmonogram[c].r, r + p)
 
-        NSetD2 = copy.deepcopy(NSetD)
+        NSetR2 = copy.deepcopy(NSet)
+        NSetR3 = copy.deepcopy(NSet)
 
-        LB = schrageDistribution.SchrageWithDistributuion(NSetD)
+        LB = schrageDistribution.SchrageWithDistributuion(NSetR2)
 
         if LB < UB:
-            print("Funkcja 1 U: ", U, " UB: ", UB)
-            Calier(NSetD2, UB)
 
-        # Harmonogram[c].r = Harmonogram_c_r
+            Calier(NSetR3)
 
-        Harmonogram_c_q = copy.copy(Harmonogram[c].q)
+        NSet[index].r = original[index].r
+
+        indexq = 0
 
         for i in range(0, int(n)):
             if NSet[i].p == Harmonogram[c].p and NSet[i].r == Harmonogram[c].r:
+                indexq = i
                 NSet[i].q = max(Harmonogram[c].q, q + p)
 
-        NSet1 = copy.deepcopy(NSet)
+        NSetQ1 = copy.deepcopy(NSet)
+        NSetQ2 = copy.deepcopy(NSet)
 
-        LB = schrageDistribution.SchrageWithDistributuion(NSet)
+        LB = schrageDistribution.SchrageWithDistributuion(NSetQ1)
+
         if LB < UB:
-            print("Funkcja 2 U: ", U, " UB: ", UB)
-            Calier(NSet1, UB)
+            Calier(NSetQ2)
 
-        # Harmonogram[c].q = Harmonogram_c_q
-
+        NSet[indexq].q = original[indexq].q
 
 
-Nset=[]
 filename = "SCHRAGE3.DAT"
 [Nset, n] = readFile.readFile(filename)
 
 UB = math.inf
 
-x = Calier(Nset, UB)
-print("efekt: ", x)
-
-
-
+Calier(Nset)
 
