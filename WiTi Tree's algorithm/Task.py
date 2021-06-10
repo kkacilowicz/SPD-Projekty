3b965e71abc5d337d0ad164627462ca78c2400c0
kkacilowicz <kacperowekar@gmail.com>
2021-06-10 02:33:53 +0200

    WiTi Tree's algortihm ready

--- WiTi Tree's algorithm/Task.py
+++ WiTi Tree's algorithm/Task.py
@@ -1,14 +1,21 @@
 # Class which contains info about each task
 class Task:
     # Constructor of class
-    def __init__(self, p, d, w):
-        # r - time of accessibility
+    def __init__(self, p, w, d):
         # p - time of execution
-        # q - time of deliver the task
+        # w - punishment rate
+        # d - requested time of execution end
         self.p = int(p)
-        self.d = int(d)
         self.w = int(w)
+        self.d = int(d)
 
     # This is a definition used in printing an object
     def __str__(self):
-        return "p: %d d: %d w: %d" % (self.p, self.d, self.w)
+        return "p: %d w: %d d: %d" % (self.p, self.w, self.d)
+
+
+# Compute punishment
+# If task wasn't late punishment is 0
+# If task is late then it is (C - d) * w
+def punishment(task, EndOfExecution):
+    return max((EndOfExecution - task.d) * task.w, 0)
--- WiTi Tree's algorithm/WiTi.py
+++ WiTi Tree's algorithm/WiTi.py
@@ -1,6 +1,50 @@
 import csv
 
-from Task import Task
+from Task import Task, punishment
+
+
+def WiTi(tasks):
+    # Number of OptimalPunishments is 2^n, where n is number of tasks
+    # pow(2, len(task)) f. ex. if theres 3 tasks looks like that 00001000
+    NumberOfCombinations = pow(2, len(tasks))
+
+    # This variable is just to initialize array with random values
+    # (in C++ we would do int tab = new int[NumberOfCombinations])
+    n = 123456
+    # In this array we store values of punishment of each iteration
+    OptimalPunishments = [n] * NumberOfCombinations
+    # We never use OptimalPunishments of 0
+    OptimalPunishments[0] = 0
+
+    # for each combination
+    for i in range(1, NumberOfCombinations):
+
+        # End of execution time of that task
+        EndOfExecution = 0
+        # Just a big value so as the algorithm works fine
+        OptimalPunishments[i] = 10000000
+
+        # Compute Cmax for this combination
+        for j in range(0, len(tasks)):
+            # If task is within this combination
+            # Exactly, check if j bit within i is set
+            # f ex if is 00001010 then it is true for j = 1 and j=3
+            if i & (1 << j):
+                # Add time of execution to Cmax
+                EndOfExecution += tasks[j].p
+
+        # Compute punishments
+        for j in range(0, len(tasks)):
+            # If task is within this combination
+            if i & (1 << j):
+                # Bit operator ~ means NOT
+                # Check OPT for combination without this task and add punishment
+                tmp = OptimalPunishments[i & (~(1 << j))] + punishment(tasks[j], EndOfExecution)
+                # If it's smallest, then it is optimal
+                OptimalPunishments[i] = min(OptimalPunishments[i], tmp)
+
+    # return Optimal value for execution of all tasks
+    return OptimalPunishments[NumberOfCombinations - 1]
 
 
 # It's a list for unordered tasks (not ready to be executed)
@@ -17,6 +61,5 @@ if file.readable():
         Tasks.append(Task(row[0], row[1], row[2]))
     file.close()
 
-for s in Tasks:
-    print(s)
-
+result = WiTi(Tasks)
+print(result)
